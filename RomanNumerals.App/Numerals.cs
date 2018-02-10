1910940205273d9dbf48ca1f80ad124c2cd9273d
Ben Harrington <harringtonben@icloud.com>
2018-02-10 12:37:01 -0600

    finished 7 of the tests

--- RomanNumerals.App/Numerals.cs
+++ RomanNumerals.App/Numerals.cs
@@ -14,25 +14,41 @@ namespace RomanNumerals.App
 
             while (number > 0)
             {
+                if (number >= 40)
+                {
+                    numeralReturn += "XL";
+                    number -= 40;
+                    continue;
+                }
                 if (number >= 10)
                 {
                     numeralReturn += "X";
                     number -= 10;
+                    continue;
                 }
-                if (number == 9)
+                if (number >= 9)
                 {
                     numeralReturn += "IX";
                     number -= 9;
+                    continue;
                 }
-                if (number == 5)
+                if (number >= 5)
                 {
                     numeralReturn += "V";
                     number -= 5;
+                    continue;
+                }
+                if (number >= 4)
+                {
+                    numeralReturn += "IV";
+                    number -= 4;
+                    continue;
                 }
                 if (number >= 1)
                 {
                     numeralReturn += "I";
                     number -= 1;
+                    continue;
                 }
             }
 
--- RomanNumerals.Test/RomanNumeralsTest.cs
+++ RomanNumerals.Test/RomanNumeralsTest.cs
@@ -65,5 +65,50 @@ namespace RomanNumerals.Test
             //Assert
             Assert.AreEqual("XII", result);
         }
+
+        [Test]
+        public void If_I_Enter_a_16_then_I_should_get_a_XVI_back()
+        {
+            //Arrange
+            var number = "16";
+            var convertNumber = int.Parse(number);
+            var numeral = new Numerals();
+
+            //Act
+            var result = numeral.ConvertNumber(convertNumber);
+
+            //Assert
+            Assert.AreEqual("XVI", result);
+        }
+
+        [Test]
+        public void If_I_Enter_a_29_then_I_should_get_a_XXIX_back()
+        {
+            //Arrange
+            var number = "29";
+            var convertNumber = int.Parse(number);
+            var numeral = new Numerals();
+
+            //Act
+            var result = numeral.ConvertNumber(convertNumber);
+
+            //Assert
+            Assert.AreEqual("XXIX", result);
+        }
+
+        [Test]
+        public void If_I_Enter_a_44_then_I_should_get_a_XLIV_back()
+        {
+            //Arrange
+            var number = "44";
+            var convertNumber = int.Parse(number);
+            var numeral = new Numerals();
+
+            //Act
+            var result = numeral.ConvertNumber(convertNumber);
+
+            //Assert
+            Assert.AreEqual("XLIV", result);
+        }
     }
 }
