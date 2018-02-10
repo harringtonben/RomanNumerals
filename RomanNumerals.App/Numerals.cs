695374d1f9b59845d1b0686b1eae50a2706aea06
Ben Harrington <harringtonben@icloud.com>
2018-02-10 11:52:50 -0600

    finsihed the first test

---
+++ RomanNumerals.App/Numerals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals.App
+{
+    public class Numerals
+    {
+        public string ConvertNumber(int number)
+        {
+            if (number >= 1) return "I";
+            return "You must enter a valid number";
+        }
+    }
+
+}
---
+++ RomanNumerals.Test/RomanNumeralsTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using RomanNumerals.App;
+
+namespace RomanNumerals.Test
+{
+    [TestFixture]
+    public class RomanNumeralsTest
+    {
+        [Test]
+        public void If_I_Enter_a_1_then_I_should_get_a_one_back()
+        {
+            //Arrange
+            var number = "1";
+            var convertNumber = int.Parse(number);
+            var numeral = new Numerals();
+
+            //Act
+            var result = numeral.ConvertNumber(convertNumber);
+
+            //Assert
+            Assert.AreEqual("I", result);
+        }
+    }
+}
